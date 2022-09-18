292260c8abb17aa23494e440884f3f23c8127ccf
DDouglas <53360057+ddouglas86@users.noreply.github.com>
2022-09-18 13:13:42 -0500

    set up routes

--- server.js
+++ server.js
@@ -1,4 +1,25 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const api = require('./routes/index.js');
 
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.get('/', (req, res) => 
+res.sendFile(path.join(_dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+res.sendFile(path.join(_dirname, '/public/notes.html'))
+);
+
+app.listen(PORT, () =>
+console.log('Listening at http://localhost:${PORT}')
+);
